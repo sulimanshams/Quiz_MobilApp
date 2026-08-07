f21343933748461bec9b6ff4aaffe410ac105d4c
suliman shams <sulimanshamsn191@gmail.com>
2026-08-07 23:35:50 +0430

    rendiring data dynamicly

--- adv_basics_quizapp/lib/answer_button.dart
+++ adv_basics_quizapp/lib/answer_button.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  AnswerButton(this.answerText ,{super.key});
+  AnswerButton(this.answerText, this.onTap ,{super.key});
 
   final String answerText;
+  final void Function() onTap;
 
   @override
   Widget build(contxt) {
     return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(),
-      child: Text(answerText),
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10 , horizontal: 40),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))
+      ),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+        
+        ),
     );
   }
 }
--- adv_basics_quizapp/lib/questions_screen.dart
+++ adv_basics_quizapp/lib/questions_screen.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics_quizapp/answer_button.dart';
 import 'package:flutter/material.dart';
+import "package:adv_basics_quizapp/data/questions.dart";
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -8,45 +10,64 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentquestionIndex = 0;
 
+  void answerQuestion() {
+    setState(() {
+      currentquestionIndex = currentquestionIndex + 1;
+    });
+  }
 
+  @override
+  Widget build(context) {
+    final currentQuestion = questions[currentquestionIndex];
 
-class _QuestionsScreenState extends State<QuestionsScreen>{
-   Widget build(context){
-     return SizedBox(
+    return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "The question text will go here!",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              // "The question text will go here!",
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              textAlign: TextAlign.center,
             ),
-           ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {}, 
-            child: Text("Answer 1"),
-            ),
+            SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answer, answerQuestion);
+            }),
+            // AnswerButton(currentQuestion.answers[0], () {}),
+            // AnswerButton(currentQuestion.answers[1], () {}),
+            // AnswerButton(currentQuestion.answers[2], () {}),
+            // AnswerButton(currentQuestion.answers[3], () {}),
 
-             ElevatedButton(
-            onPressed: () {}, 
-            child: Text("Answer 2"),
-            ),
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: Text("Answer 1"),
+            //   ),
 
-             ElevatedButton(
-            onPressed: () {}, 
-            child: Text("Answer 3"),
-            ),
+            //    ElevatedButton(
+            //   onPressed: () {},
+            //   child: Text("Answer 2"),
+            //   ),
 
-             ElevatedButton(
-            onPressed: () {}, 
-            child: Text("Answer 4"),
-            ),
-        ],
+            //    ElevatedButton(
+            //   onPressed: () {},
+            //   child: Text("Answer 3"),
+            //   ),
+
+            //    ElevatedButton(
+            //   onPressed: () {},
+            //   child: Text("Answer 4"),
+            //   ),
+          ],
+        ),
       ),
-      
     );
-}}
+  }
+}
